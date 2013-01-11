fd02cfb1b8e804a18c9bdc8413fa11ab93c48c53
Patrick Armitage <patrick.armitage@liferay.com>
2013-01-11 15:53:40 -0800

    Testing Javascript

--- src/documents/examples/portal-layout/real-world.js
+++ src/documents/examples/portal-layout/real-world.js
@@ -1,6 +1,8 @@
 AUI().use('aui-portal-layout', function(A) {
   var proxyNode = A.Node.create('<div class="aui-portal-layout-proxy"></div>');
   var sb = A.one('.sidebar');
+  var DDM = A.DD.DDM;
+  var color = '';
 
   var portalLayout = new A.PortalLayout({
     dragNodes: '.portlet',
@@ -9,13 +11,10 @@ AUI().use('aui-portal-layout', function(A) {
     validDrops: [ '#myPortalLayout' ]
   });
 
-  //New Portlet Generation
-  var DDM = A.DD.DDM;
-  var color = '';
-
   var PortletItem = function() {
     PortletItem.superclass.constructor.apply(this, arguments);
   };
+  window.porti = PortletItem;
 
   PortletItem.NAME = 'PortletItem';
   PortletItem.ATTRS = {
@@ -27,12 +26,6 @@ AUI().use('aui-portal-layout', function(A) {
     },
     proxyNode: {
       value: proxyNode
-    },
-    isTarget: {
-      value: false
-    },
-    validDrops: {
-      value: [ '#myPortalLayout' ]
     }
   };
 
@@ -62,5 +55,24 @@ AUI().use('aui-portal-layout', function(A) {
       );
     }
   });
-  DDM.removeTarget(10);
+
+  rmDrops;
+  function rmDrops() {
+    var targets = A.Object.values(DDM.targets);
+
+    for(i = 0; i<targets.length; i++) {
+      var node = targets[i];
+
+      if(node._state.data.node.value.hasClass('nodrop')) {
+        var nodrop = node._groups;
+        A.Object.setValue(nodrop, ['portal-layout'], false);
+        console.log(nodrop);
+      }
+    }
+  };
+
+  //console.log(A.Object.values(A.DD.DDM.targets));
+  setTimeout( rmDrops, 100);
+  //rmDrops();
+
 });
